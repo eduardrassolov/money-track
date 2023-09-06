4a0d281bccbbe9d2ade055dc2ff8ae598b4aa9bd
eduardrassolov <eduard.rassolov@outlook.com>
2023-09-06 23:00:19 +0300

    try to deploy

--- src/pages/layout/ProtectedLayout.tsx
+++ src/pages/layout/ProtectedLayout.tsx
@@ -2,12 +2,12 @@ import React, { FC } from "react";
 import { useUser } from "../../utils/hooks/useUser";
 import Login from "../login/Login.page";
 
+
 interface IProtected {
     children: React.ReactNode;
 }
 
 const ProtectedLayout: FC<IProtected> = ({ children }) => {
-
     const { isAuthenticated } = useUser();
 
     if (!isAuthenticated) {
