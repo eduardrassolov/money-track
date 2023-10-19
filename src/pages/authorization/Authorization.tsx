4a2922184e3e41bd823e642a1d8b4b0e13075760
eduardrassolov <eduard.rassolov@outlook.com>
2023-10-19 21:58:40 +0300

    refactor

--- src/pages/authorization/Authorization.tsx
+++ src/pages/authorization/Authorization.tsx
@@ -1,42 +1,7 @@
 import { Outlet } from 'react-router-dom';
-import styled, { ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components';
 import useTheme from "../../utils/hooks/useTheme.tsx";
-import { devices } from '../../styles/breakPoints.ts';
-
-const StyledAuthorization = styled.div`
-    background: ${props => props.theme.background};
-    color: ${props => props.theme.text};
-    height: 100vh;
-    justify-content: center;
-    align-items: center;
-
-    @media only screen and (min-width: ${devices.sm}px){
-        display: grid;
-        grid-template-columns: 1fr 1fr;
-    }
-`
-const ImageContainer = styled.div`
-    display: none;
-
-    @media only screen and (min-width: ${devices.sm}px){  
-        img{
-            width: 100%;
-            max-width: 700px;
-        }
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        max-width: 700px;
-    }   
-
-`
-const StyledContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    padding: 2rem 1rem;
-    max-width: 600px;
-    margin: auto 0;
-`
+import { ImageContainer, StyledAuthorization, StyledContainer } from './Authorizations.style.ts';
 
 export default function AuthorizationLayout() {
     const { theme } = useTheme();
---
+++ src/pages/authorization/Authorizations.style.ts
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+import { devices } from "../../styles/breakPoints";
+
+export const StyledAuthorization = styled.div`
+    background: ${(props) => props.theme.background};
+    color: ${(props) => props.theme.text};
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+
+    @media only screen and (min-width: ${devices.sm}px) {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+    }
+`;
+
+export const ImageContainer = styled.div`
+    display: none;
+
+    @media only screen and (min-width: ${devices.sm}px) {
+        img {
+        width: 100%;
+        max-width: 700px;
+        }
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        max-width: 700px;
+    }
+`;
+
+export const StyledContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding: 2rem 1rem;
+    max-width: 600px;
+    margin: auto 0;
+`;
