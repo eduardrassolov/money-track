2d27308c9bfeffd93a38602cb319122655081b8f
eduardrassolov <eduard.rassolov@outlook.com>
2023-11-29 20:34:17 +0200

    added navbar to main page

--- src/pages/home/Home.page.tsx
+++ src/pages/home/Home.page.tsx
@@ -1,22 +1,23 @@
 import { ThemeProvider } from "styled-components";
-// import useBurgerMenu from "../../components/navBarApp/useBurger";
+import useBurgerMenu from "../../components/navBarApp/useBurger";
 import useTheme from "../../utils/hooks/useTheme";
 import { Container } from "./Home.page.style";
 
 // import Footer from "./footer/Footer";
 // import FeaturesSection from "./features/FeaturesSection";
 import HeaderSection from "./header/HeaderSection";
+import NavBarHome from "./navbar/NavBarHome";
 // import NavBarHome from "./navBar/NavBarHome";
 
 export default function HomePage() {
-    // const { isBurgerOpen, handleBurger } = useBurgerMenu();
+    const { isBurgerOpen, handleBurger } = useBurgerMenu();
     const { theme } = useTheme();
 
     return (
         <ThemeProvider theme={theme}>
             <Container>
 
-                {/* <NavBarHome isOpen={isBurgerOpen} onClose={handleBurger} /> */}
+                <NavBarHome isOpen={isBurgerOpen} onClose={handleBurger} />
 
                 <HeaderSection id={"header"} />
 
