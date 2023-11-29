f7d760ff35f76f90b689ab4315641aaf5b050cb7
eduardrassolov <eduard.rassolov@outlook.com>
2023-11-29 22:15:33 +0200

    to be checked

--- src/pages/home/Home.page.tsx
+++ src/pages/home/Home.page.tsx
@@ -1,29 +1,32 @@
 import { ThemeProvider } from "styled-components";
-// import useBurgerMenu from "../../components/navBarApp/useBurger";
+import useBurgerMenu from "../../components/navBarApp/useBurger";
 import useTheme from "../../utils/hooks/useTheme";
 import { Container } from "./Home.page.style";
-
-// import Footer from "./footer/Footer";
-// import FeaturesSection from "./features/FeaturesSection";
+//@ts-ignore
+import Footer from "./footer/Footer";
+//@ts-ignore
+import FeaturesSection from "./features/FeaturesSection";
 import HeaderSection from "./header/HeaderSection";
-// import NavbarHome from "./navbar/NavbarHome"; 
+//@ts-ignore
+import NavbarHome from "./navbar/NavbarHome";
+//@ts-ignore
 import FooterContainer from "./footer/Footer";
 
 // import NavBarHome from "./navBar/NavBarHome";
 
 export default function HomePage() {
-    // const { isBurgerOpen, handleBurger } = useBurgerMenu();
+    const { isBurgerOpen, handleBurger } = useBurgerMenu();
     const { theme } = useTheme();
 
     return (
         <ThemeProvider theme={theme}>
             <Container>
 
-                {/* <NavbarHome isOpen={isBurgerOpen} onClose={handleBurger} /> */}
+                <NavbarHome isOpen={isBurgerOpen} onClose={handleBurger} />
 
                 <HeaderSection id={"header"} />
 
-                {/* <FeaturesSection id={"feature"} />*/}
+                <FeaturesSection id={"feature"} />
 
                 <FooterContainer />
             </Container>
